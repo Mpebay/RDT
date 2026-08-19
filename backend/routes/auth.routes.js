f552c07359e5ceb66026a51dc95cbc8203006825
Manuel Pebay <manupebay@hotmail.com>
2026-08-19 15:51:27 -0300

    Restablecer contraseña correcto 1.0

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -6,5 +6,7 @@ const { protect } = require('../middleware/auth.middleware');
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/profile', protect, getUserProfile); // <-- Nueva ruta de sincronización
+router.post('/forgot-password', require('../controllers/auth.controller').forgotPassword);
+router.post('/reset-password/:token', require('../controllers/auth.controller').resetPassword);
 
 module.exports = router;
