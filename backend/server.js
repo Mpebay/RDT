b3aca9cef91218e33996e42c51e02b64dd40f843
Manuel Pebay <manupebay@hotmail.com>
2026-08-19 15:45:48 -0300

    Restablecer contraseña correcto

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,23 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/auth.routes');
 const adminRoutes = require('./routes/admin.routes');
-const courseRoutes = require('./routes/course.routes'); // <-- NUEVO
+const courseRoutes = require('./routes/course.routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-// Rutas
+// Rutas de la API
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
-app.use('/api/courses', courseRoutes); // <-- NUEVO
+app.use('/api/courses', courseRoutes);
 
+// Conexión a Base de Datos y arranque del servidor
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB Conectado');
-    app.listen(process.env.PORT, () => console.log(`Servidor corriendo en puerto ${process.env.PORT}`));
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
   })
-  .catch(err => console.log(err));
+  .catch(err => console.log('Error conectando a MongoDB:', err));
