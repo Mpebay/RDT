22dd0cf545c37f00b020a852d4e7c77af5de21b8
Manuel Pebay <manupebay@hotmail.com>
2026-08-25 14:45:13 -0300

    act admin.routes

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers, approveUser, deleteUser, assignRole } = require('../controllers/admin.controller');
-const { protect, admin } = require('../middleware/auth.middleware');
+const { 
+  getUsers, 
+  approveUser, 
+  updateUserPlan, 
+  deleteUser, 
+  assignRole 
+} = require('../controllers/adminController');
+const { protect, admin } = require('../middleware/authMiddleware');
 
 router.get('/users', protect, admin, getUsers);
 router.put('/users/:id/approve', protect, admin, approveUser);
-router.delete('/users/:id', protect, admin, deleteUser);
-
+router.put('/users/:id/plan', protect, admin, updateUserPlan);
 router.put('/users/:id/role', protect, admin, assignRole);
+router.delete('/users/:id', protect, admin, deleteUser);
 
 module.exports = router;
