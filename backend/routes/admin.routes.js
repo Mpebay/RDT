ed48b77304498825464f054c718040ad6b0bee17
Manuel Pebay <manupebay@hotmail.com>
2026-08-25 14:53:34 -0300

    act admin.routes 1.1

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -7,7 +7,7 @@ const {
   deleteUser, 
   assignRole 
 } = require('../controllers/admin.controller');
-const { protect, admin } = require('../middleware/authMiddleware');
+const { protect, admin } = require('../middleware/auth.middleware);');
 
 router.get('/users', protect, admin, getUsers);
 router.put('/users/:id/approve', protect, admin, approveUser);
